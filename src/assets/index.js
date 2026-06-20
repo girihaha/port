28ad38505575ada60b8d06880c86a83430c9d5b1
girihaha <yeuha.lama.na@gmail.com>
2026-06-20 14:37:35 +0530

    Add various tech icons to index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,40 @@
 import logo from "./logo.svg";
+
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+
 import perplexity from "./Perplexity-AI-Partnership-with-SoftBank-for-Japan-Expansion.jpg";
 
+// Skills icons
+import html from "./tech/html.png";
+import css from "./tech/css.png";
+import javascript from "./tech/javascript.png";
+import typescript from "./tech/typescript.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import nodejs from "./tech/nodejs.png";
+import mongodb from "./tech/mongodb.png";
+import threejs from "./tech/threejs.svg";
+import nextjs from "./tech/nextjs.png";
+import express from "./tech/express.png";
+import java from "./tech/java.png";
+import git from "./tech/git.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import jwt from "./tech/jwt.png";
+import nextauth from "./tech/nextauth.png";
 
 export {
   logo,
@@ -24,5 +45,29 @@ export {
   github,
   menu,
   close,
+
+  meta,
+  shopify,
+  starbucks,
+  tesla,
   perplexity,
+
+  html,
+  css,
+  javascript,
+  typescript,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  threejs,
+  nextjs,
+  express,
+  java,
+  git,
+  docker,
+  figma,
+  jwt,
+  nextauth,
 };
